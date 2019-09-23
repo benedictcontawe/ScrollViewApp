ed5432641c0397c5d64bfb5d3f2535a0551254e9
Benedict <benedict.b.contawe@accenture.com>
2019-09-23 15:26:15 +0800

    Update animation effect add new drawables

--- app/src/main/java/com/example/scrollviewapp/MainActivity.java
+++ app/src/main/java/com/example/scrollviewapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.scrollviewapp;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewTreeObserver;
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
 
     CheckBox chck_switch;
     NestedScrollView nsv_main;
+    View fog_top, fog_bottom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +24,8 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
 
         chck_switch = (CheckBox) findViewById(R.id.chck_switch);
         nsv_main = (NestedScrollView) findViewById(R.id.nsv_main);
+        fog_top = (View) findViewById(R.id.view_fog_top);
+        fog_bottom = (View) findViewById(R.id.view_fog_bottom);
 
         nsv_main.setOnTouchListener(this);
         nsv_main.getViewTreeObserver().addOnScrollChangedListener(this);
@@ -38,11 +42,19 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         int topDetector = nsv_main.getScrollY();
         int bottomDetector = view.getBottom() -  (nsv_main.getHeight() + nsv_main.getScrollY());
 
-        if(bottomDetector <= 0 ){
-            Toast.makeText(getBaseContext(),"Nested Scroll View bottom reached",Toast.LENGTH_SHORT).show();
+        if(topDetector <= 0) {
+            //Toast.makeText(getBaseContext(),"Nested Scroll View top reached",Toast.LENGTH_SHORT).show();
+            Log.d(MainActivity.class.getSimpleName(),"Nested Scroll View top reached");
+            fog_top.setVisibility(View.INVISIBLE);
         }
-        if(topDetector <= 0){
-            Toast.makeText(getBaseContext(),"Nested Scroll View top reached",Toast.LENGTH_SHORT).show();
+        else if(bottomDetector <= 0 ) {
+            //Toast.makeText(getBaseContext(),"Nested Scroll View bottom reached",Toast.LENGTH_SHORT).show();
+            Log.d(MainActivity.class.getSimpleName(),"Nested Scroll View bottom reached");
+            fog_bottom.setVisibility(View.INVISIBLE);
+        }
+        else {
+            fog_top.setVisibility(View.VISIBLE);
+            fog_bottom.setVisibility(View.VISIBLE);
         }
     }
 }
