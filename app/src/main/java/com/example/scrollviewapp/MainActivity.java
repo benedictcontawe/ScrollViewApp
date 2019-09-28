906bc15357fd826a080e51c72db3c857f3116aa2
Benedict <benedictcontawe@gmail.com>
2019-09-28 20:27:02 +0800

    cleaning codes

--- app/src/main/java/com/example/scrollviewapp/MainActivity.java
+++ app/src/main/java/com/example/scrollviewapp/MainActivity.java
@@ -6,7 +6,6 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewTreeObserver;
 import android.widget.CheckBox;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.widget.NestedScrollView;
